2321e5a878f37385fe8741ec23cb29ba587b415a
Edin Tufek <edintufek11@gmail.com>
2025-10-15 18:15:01 +0200

    created backend

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+   const config = new DocumentBuilder()
+    .setTitle('Test API')
+    .setDescription('The test API description')
+    .setVersion('1.0')
+    .addTag('test')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
